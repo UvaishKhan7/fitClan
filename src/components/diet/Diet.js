17490470979ff675ca9d0fd3328ea640de49de9d
Habeeb8136 <habeeb041099@gmail.com>
2022-12-26 17:03:31 +0530

    diet page

--- src/components/diet/Diet.js
+++ src/components/diet/Diet.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { UserAuth } from '../../UserAuthContext';
 import './Diet.css'
 import foodData from '../../foodData.json'
@@ -7,12 +7,39 @@ import CustomDiet from '../CustomDiet/CustomDiet';
 export default function Diet() {
 
     const {user} = UserAuth();
+    const [showPlan, setShowPlan] = useState(false);
 
+    function showDietMaker(){
+      setShowPlan(!showPlan)
+    }
   return (
     <div className='diet  p-5'>
-        <h2>Hello {user && user.email}</h2>
-        <h2>Calculate your Calorie intake and create your own diet plan</h2>  
-        <CustomDiet foodData={foodData}/>    
+        <h2>Common diet plans</h2>
+        <div className='commonDiet'>
+
+          <div className='row1'>
+          <div className='plan1 plan'>plan1</div>
+          <div className='plan2 plan'>plan2</div>
+          </div>
+
+          <div className='row2'>
+          <div className='plan1 plan'>plan1</div>
+          <div className='plan2 plan'>plan2</div>
+          </div>
+
+          <div className='row3'>
+          <div className='plan1 plan'>plan1</div>
+          <div className='plan2 plan'>plan2</div>
+          </div>
+
+        </div>
+        <br></br>
+        <div className='dietHeading' id='createplan'>
+        {!showPlan?<h2>Want to create your own diet plan?</h2>:<h2>Custom plan</h2>}
+        {!showPlan && <a href='#createplan'><div className='createPlan' onClick={showDietMaker}>Create</div></a> }
+         
+        </div>
+        {showPlan && <CustomDiet foodData={foodData}/>} 
     </div>
   )
 }
