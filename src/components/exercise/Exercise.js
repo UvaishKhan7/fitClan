76ad54e368a14a4fb2900e1273127dbdace002bf
UvaishKhan7 <uvaish8248@gmail.com>
2023-01-11 18:57:13 +0530

    added exercise plans

--- src/components/exercise/Exercise.js
+++ src/components/exercise/Exercise.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './exercise.css';
+import { collection, onSnapshot, query } from 'firebase/firestore';
+import db from '../../firebase';
 
 export default function Exercise() {
+
+  const [exercise, setExercise] = useState([{}]);
+
+  useEffect(() => {
+    const foodRef = collection(db, "exercise");
+    const q = query(foodRef);
+    onSnapshot(q, (snapshot) => {
+      setExercise(snapshot.docs.map((doc) =>
+      ({
+        ...doc.data(),
+        id: doc.id
+      })
+      ))
+    });
+  }, [])
+
   return (
     <div className='exercise'>
       <h3>Your exercise plan is below:</h3>
-      <div className="exercise_plans">
-        <h3>Exercise Plans</h3>
-        <div className="exercise_plans_container">
-          <div className="goal_plan">
-          </div>
-        </div>
+      <div className="exercise_outer">
+        {
+          exercise?.map(item => (
+            <div key={item.id} className="exercise_plans">
+              <h3>{item.title} &nbsp;
+                <small>({item.category})</small>
+              </h3>
+              <div className="exercise_plans_container">
+                <p className="goal_plan">
+                  {item.details}
+                </p>
+              </div>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
