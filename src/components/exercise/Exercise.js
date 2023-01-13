af95b3676589c6f63e7c188e1ac0767778380037
Habeeb Muhammad Arif <habeeb041099@gmail.com>
2023-01-13 15:59:07 +0530

    added search exercise

--- src/components/exercise/Exercise.js
+++ src/components/exercise/Exercise.js
@@ -1,13 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './exercise.css';
 import { collection, onSnapshot, query } from 'firebase/firestore';
 import db from '../../firebase';
 import { Skeleton } from '@mui/material';
+import Model from 'react-body-highlighter';
+import { BsSearch } from 'react-icons/bs';
+import ExerciseCard from './ExerciseCard';
 
 export default function Exercise() {
 
   const [exercise, setExercise] = useState([{}]);
   const [loading, setLoading] = useState(true);
+  const [exerciseData, setExerciseData] = useState([]);
+  const [searchedData, setsearchedData] = useState([]);
+  const [wordEntered, setWordEntered] = useState('');
+
+  const searchResult = useRef(null)
 
   useEffect(() => {
     setLoading(true);
@@ -23,8 +31,46 @@ export default function Exercise() {
     });
     setInterval(() => {
       setLoading(false);
-    }, 2500)
-  }, [])
+    }, 3500)
+  }, []);
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '5ef7f53944mshe6f63588c528930p182765jsn1b4484d497b8',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://exercisedb.p.rapidapi.com/exercises', options)
+      .then(response => response.json())
+      .then(res => {
+        setExerciseData(res)
+        console.log(res)
+      })
+                .catch(err => console.error(err));
+    
+  }, []);
+
+  
+
+  const handleSearch = (e) => {
+      
+    const filteredData=exerciseData.filter(
+      (item) => item.name.includes(wordEntered)
+      || item.target.includes(wordEntered)
+      || item.equipment.includes(wordEntered)
+      || item.bodyPart.includes(wordEntered));
+      
+              setWordEntered('')
+             setsearchedData(filteredData);
+             console.log('searchedword',wordEntered)
+             window.scrollTo({
+              top: searchResult.current.offsetTop,
+              behavior: 'smooth',
+            });
+    };
 
   return (
     <div className='exercise'>
@@ -64,6 +110,52 @@ export default function Exercise() {
           ))
         }
       </div>
+
+
+      <div className='search-exercise'>
+      <h2 >Search Exercises For Details</h2>
+
+      <div className='choose-target'>
+      <h3>select by Target</h3>
+      <Model  
+        
+        style={{ width:'40%',padding: '1rem' }}
+        highlightedColors={["#ff0000", "#0000ff"]}
+        
+      />
+        <Model
+          type="posterior"
+          style={{width:'40%', padding: '1rem' }}
+          
+          highlightedColors={["#ff0000", "#db2f2f"]}
+          
+        />
+      </div>
+        </div>
+      
+
+      <div className='search-results' >
+      <div className="searchbar-exercises" ref={searchResult}>
+        <div className="searchinput-exercise">
+          <input className='input-exercise'
+          onKeyPress={(e) => {
+                        if (e.key === "Enter"){
+                          handleSearch()
+                        } }}
+           value={wordEntered}
+          onChange={e=>{setWordEntered(e.target.value)}}
+          placeholder="Search Exercises/Body part/Target .."
+          type="text"
+          >
+          </input>
+          <BsSearch className="searchicon" onClick={handleSearch}/>
+        </div>
+        </div>
+        {searchedData !== 0 && (
+          <ExerciseCard searchedData={searchedData}/>
+        )}
+     </div>
+
     </div>
   )
 }
