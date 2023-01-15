62f75424da6950504209d886bc55de08000425b1
Habeeb8136 <habeeb041099@gmail.com>
2023-01-15 15:47:29 +0530

    restyled exercisecard

--- src/components/exercise/ExerciseCard.js
+++ src/components/exercise/ExerciseCard.js
@@ -3,13 +3,13 @@ import './exerciseCard.css'
 import { BiBody } from 'react-icons/bi'
 import { CgGym } from 'react-icons/cg'
 
-const ExerciseCard = ({ searchedData }) => {
+const ExerciseCard = ({ exercise }) => {
 
   return (
-    <div className="card-wrapper">
-      {searchedData.splice(0, 15).map((exercise, index) => {
-        return (
-          <div key={index} className="exercise-card" >
+    
+    !exercise ? null :
+    
+          <div className="exercise-card" >
             <div className='gif'><img src={exercise.gifUrl} alt={exercise.name}></img></div>
             <div className='exerscise-details'>
               <div className='exercise-name'>{exercise.name}</div>
@@ -18,11 +18,8 @@ const ExerciseCard = ({ searchedData }) => {
                 <div className='badge'><CgGym /> {exercise.equipment}</div>
               </div>
             </div>
-
           </div>
-        );
-      })}
-    </div>
+    
   );
 }
 
