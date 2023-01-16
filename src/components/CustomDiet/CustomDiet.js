05ab38aa1da57a4e341c513b01d046ad27a18eca
Habeeb8136 <habeeb041099@gmail.com>
2023-01-16 18:54:16 +0530

    setintake passed from diet.js

--- src/components/CustomDiet/CustomDiet.js
+++ src/components/CustomDiet/CustomDiet.js
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import './CustomDiet.css';
-import db from '../../firebase';
-import { addDoc, collection, deleteDoc, doc, onSnapshot, orderBy, query, serverTimestamp } from 'firebase/firestore';
-import { UserAuth } from '../../UserAuthContext';
+import React, { useEffect, useState } from "react";
+import "./CustomDiet.css";
+import db from "../../firebase";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
+import { UserAuth } from "../../UserAuthContext";
 import { BsSearch } from "react-icons/bs";
-import { HiOutlineTrash } from 'react-icons/hi'
-import { Button } from '@mui/material';
+import { HiOutlineTrash } from "react-icons/hi";
+import { Button } from "@mui/material";
 
-export default function CustomDiet({ id, title, time , meals }) {
-
+export default function CustomDiet({ id, title, time, meals, setIntake }) {
   const [foodItems, setFoodItems] = useState([]);
   const [foodData, setFoodData] = useState([]);
   const [searchedData, setsearchedData] = useState([]);
@@ -31,17 +40,23 @@ export default function CustomDiet({ id, title, time , meals }) {
 
   // adding new food item to the meal & database
   const additem = async (item) => {
-
     if (item) {
-      const mealColRef = collection(db, "user", user.uid, 'meals', id, `${title}`);
+      const mealColRef = collection(
+        db,
+        "user",
+        user.uid,
+        "meals",
+        id,
+        `${title}`
+      );
       await addDoc(mealColRef, {
         title: item.title,
         carbs: item.carbs,
         protein: item.proteins,
         fat: item.fat,
         calory: item.calories,
-        timestamp: serverTimestamp()
-      })
+        timestamp: serverTimestamp(),
+      });
       setsearchedData([]);
       setWordEntered("");
     }
@@ -49,77 +64,110 @@ export default function CustomDiet({ id, title, time , meals }) {
 
   // for deleting the meal
   const deleteMeal = async (id) => {
-    await deleteDoc(doc(db, "user", user.uid, 'meals', id))
-  }
+    await deleteDoc(doc(db, "user", user.uid, "meals", id));
+  };
 
   // for deleting food item
   const deleteFoodItem = async (foodId) => {
-    await deleteDoc(doc(db, "user", user.uid, 'meals', id, `${title}`, foodId))
-  }
+    await deleteDoc(doc(db, "user", user.uid, "meals", id, `${title}`, foodId));
+  };
 
-  // fetching the food items in a meal 
+  // fetching the food items in a meal
   useEffect(() => {
-    const colRef = collection(db, "user", user.uid, 'meals', id, `${title}`);
+    const colRef = collection(db, "user", user.uid, "meals", id, `${title}`);
     const q = query(colRef, orderBy("timestamp"));
     onSnapshot(q, (snapshot) => {
-      setFoodItems(snapshot.docs.map((doc) =>
-      ({
-        ...doc.data(),
-        id: doc.id,
-        name: doc.data().title,
-        carbs: doc.data().carbs,
-        protein: doc.data().protein,
-        fat: doc.data().fat,
-        calory: doc.data().calory
-      })
-      ))
+      setFoodItems(
+        snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+          name: doc.data().title,
+          carbs: doc.data().carbs,
+          protein: doc.data().protein,
+          fat: doc.data().fat,
+          calory: doc.data().calory,
+        }))
+      );
     });
+
+    const updateFoodItems = () => {
+      const mealColRef = collection(db, "user", user.uid, "totalMacros");
+      const qu = query(mealColRef);
+      onSnapshot(qu, (snapshot) => {
+        setIntake(
+          snapshot.docs.map((doc) => ({
+            ...doc.data(),
+            id: doc.id,
+          }))
+        );
+      });
+    };
+
+    updateFoodItems();
 
     const foodRef = collection(db, "foodItems");
     const qu = query(foodRef);
     onSnapshot(qu, (snapshot) => {
-      setFoodData(snapshot.docs.map((doc) =>
-      ({
-        ...doc.data(),
-        id: doc.id,
-        name: doc.data().title,
-        carbs: doc.data().carbs,
-        protein: doc.data().protein,
-        fat: doc.data().fat,
-        calory: doc.data().calory,
-        serving: doc.data().serving,
-        unit: doc.data().unit
-      })
-      ))
+      setFoodData(
+        snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+          name: doc.data().title,
+          carbs: doc.data().carbs,
+          protein: doc.data().protein,
+          fat: doc.data().fat,
+          calory: doc.data().calory,
+          serving: doc.data().serving,
+          unit: doc.data().unit,
+        }))
+      );
     });
-}, []);
+  }, [foodItems]);
 
   useEffect(() => {
-    setTotalCarbs(foodItems.reduce((sum, obj) => sum + obj.carbs, 0))
-  setTotalCalories(foodItems.reduce((sum, obj) => sum + obj.calory, 0))
-  setTotalProteins(foodItems.reduce((sum, obj) => sum + obj.protein, 0))
-  setTotalFat(foodItems.reduce((sum, obj) => sum + obj.fat, 0))
+    setTotalCarbs(foodItems.reduce((sum, obj) => sum + obj.carbs, 0));
+    setTotalCalories(foodItems.reduce((sum, obj) => sum + obj.calory, 0));
+    setTotalProteins(foodItems.reduce((sum, obj) => sum + obj.protein, 0));
+    setTotalFat(foodItems.reduce((sum, obj) => sum + obj.fat, 0));
+  }, [foodItems]);
+
+  useEffect(() => {
+    const mealColRef = doc(db, "user", user.uid, "totalMacros", `${title}`);
+    setDoc(mealColRef, {
+      totalCalory: totalCalories,
+      totalCarbs: totalCarbs,
+      totalProteins: totalProteins,
+      totalFat: totalFat,
+    });
   }, [foodItems]);
 
   return (
-    <div className='custom_diet'>
+    <div className="custom_diet">
       <div className="title_n_trash">
-        <div className='title_n_time'>
+        <div className="title_n_time">
           <h6>{title}</h6>
           <small>at {time}</small>
         </div>
-        <Button type="submit" onClick={() => deleteMeal(id)} variant="outlined" color="error">Delete Meal &nbsp;<HiOutlineTrash className='trash' /></Button>
+        <Button
+          type="submit"
+          onClick={() => deleteMeal(id)}
+          variant="outlined"
+          color="error"
+        >
+          Delete Meal &nbsp;
+          <HiOutlineTrash className="trash" />
+        </Button>
       </div>
 
       {/* food item search bar */}
       <div className="searchbar">
         <div className="searchinput">
-          <input className="mealinput"
+          <input
+            className="mealinput"
             placeholder="Search & add food items to your meal"
             value={wordEntered}
             onChange={handleSearch}
-          >
-          </input>
+          ></input>
           <BsSearch className="searchicon" />
         </div>
 
@@ -127,46 +175,58 @@ export default function CustomDiet({ id, title, time , meals }) {
           <div className="searchresults">
             {searchedData.slice(0, 10).map((item, title) => {
               return (
-                <div key={title} className="resultsList" onClick={e => additem(item)}>
-                  <span className='seaarch_food_title'>{item.title}
-                    <span style={{ fontSize: '12px', color: 'grey' }}>
+                <div
+                  key={title}
+                  className="resultsList"
+                  onClick={(e) => additem(item)}
+                >
+                  <span className="seaarch_food_title">
+                    {item.title}
+                    <span style={{ fontSize: "12px", color: "grey" }}>
                       &nbsp; ({item.serving} {item.unit})
-                    </span></span>
+                    </span>
+                  </span>
                   <br />
-                  <span className='search_food_macros' style={{ fontSize: '12px', color: 'grey' }}>({item.calories} kcal, Carbs:{item.carbs}g, Protein:{item.proteins}g, Fat:{item.fat}g )</span>
+                  <span
+                    className="search_food_macros"
+                    style={{ fontSize: "12px", color: "grey" }}
+                  >
+                    ({item.calories} kcal, Carbs:{item.carbs}g, Protein:
+                    {item.proteins}g, Fat:{item.fat}g )
+                  </span>
                 </div>
               );
             })}
           </div>
         )}
-
       </div>
 
-      {
-        foodItems?.map(food => (
-          <div key={food.id} className="food_list_item">
-            <div className='food_title'>{food.name}{food.serving}
-              <Button type="submit" onClick={() => deleteFoodItem(food.id)} variant="outlined" color="error">
-                <HiOutlineTrash className='trash' />
-              </Button>
-            </div>
-            <div className="macros">
-              <span>Carbs: {food.carbs}g, </span>
-              <span>Prot: {food.protein}g, </span>
-              <span>Fat: {food.fat}g, </span>
-              <span>Cal: {food.calory}kCal</span>
-            </div>
+      {foodItems?.map((food) => (
+        <div key={food.id} className="food_list_item">
+          <div className="food_title">
+            {food.name}
+            {food.serving}
+            <Button
+              type="submit"
+              onClick={() => deleteFoodItem(food.id)}
+              variant="outlined"
+              color="error"
+            >
+              <HiOutlineTrash className="trash" />
+            </Button>
           </div>
-        ))
-      }
+          <div className="macros">
+            <span>Carbs: {food.carbs}g, </span>
+            <span>Prot: {food.protein}g, </span>
+            <span>Fat: {food.fat}g, </span>
+            <span>Cal: {food.calory}kCal</span>
+          </div>
+        </div>
+      ))}
 
       <div className="total_macros">
-        Total Cal ={totalCalories.toFixed(2)} kCal,
-        Total Prot ={totalProteins.toFixed(2)} g,
-        Total Carb ={totalCarbs.toFixed(2)} g,
-        Total Fat ={totalFat.toFixed(2)} g
+         {totalCarbs.toFixed(2)} g {totalProteins.toFixed(2)} g {totalFat.toFixed(2)} g {totalCalories.toFixed(2)} kCal
       </div>
-
-    </div >
-  )
+    </div>
+  );
 }
